0f5b2d11bcb00d836e67ca66bf8ff1eae6a7a219
Dina Essam <dina.essam.fcis@gmail.com>
2017-05-02 01:30:47 +0200

    Add Classes and Fix Desgin

---
+++ SearchEngineFile/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngineFile
+{
+    class Category
+    {
+        String name { set; get; }
+        List<String> keywords { set; get; }
+
+
+
+    }
+}
---
+++ SearchEngineFile/UserFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngineFile
+{
+    class UserFile
+    {
+        String name { set; get; }
+        String path { set; get}
+        List<Category> FileCategories { set; get;}
+    }
+}
